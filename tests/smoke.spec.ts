f1f6510564252b65d55f5c1d999af783d041169b
Rolando Vazquez <rolando.vazquez.23@gmail.com>
2025-05-20 17:47:53 -0600

    Initial smoke test for categories

--- tests/smoke.spec.ts
+++ tests/smoke.spec.ts
@@ -50,3 +50,40 @@ test('About/Contact Info', async ({page}) => {
   await contactPage.submitForm('Rolando Vázquez', 'emailtest@email.com', 'Test message');
   await expect(page.getByRole('article')).toContainText('Hemos recibido tu peticion');
 });
+
+test('Categories smoke test', async ({page}) => {
+  await page.goto('http://localhost:3000/');
+  await page.getByRole('navigation').getByRole('link', { name: 'Categories' }).click();
+  await expect(page.getByRole('article')).toContainText('Explore Categories');
+  await expect(page.getByRole('button', { name: '+' })).toBeVisible();
+  await expect(page.getByText('Ruby on Rails')).toBeVisible();//Default category
+  await expect(page.getByRole('button', { name: 'All' })).toBeVisible();
+  await expect(page.getByRole('button', { name: 'Popular' })).toBeVisible();
+  await expect(page.getByRole('button', { name: 'Recent' })).toBeVisible();
+  await expect(page.getByRole('button', { name: 'Trending' })).toBeVisible();
+  await page.getByRole('button', { name: '+' }).click();
+  await expect(page.getByRole('button', { name: 'Sign in' })).toBeVisible();
+  const loginPage = PagesManager.getInstance(page).onLoginPage();
+
+  await loginPage.login('rolando.vazquez@hey.com', '123pum');
+  await page.getByRole('navigation').getByRole('link', { name: 'Categories' }).click();
+  await page.getByRole('button', { name: '+' }).click();
+  
+  await page.getByRole('textbox', { name: 'Name' }).fill('Categoria de prueba');
+  await page.getByRole('textbox', { name: 'Name' }).press('Tab');
+  await page.getByRole('textbox', { name: 'Description' }).fill('Esta es una categoria creada por la automatizacion');
+  await page.getByRole('checkbox', { name: 'Recent' }).check();
+  await page.getByRole('button', { name: 'Create Category' }).click();
+  await expect(page.getByRole('article')).toContainText('Category was successfully created.');
+  await page.getByRole('link', { name: 'Back to categories' }).click();
+  await expect(page.getByText('Categoria de Prueba')).toBeVisible();
+  await page.getByRole('button', { name: 'Trending' }).click();
+  await page.waitForResponse('http://localhost:3000/categories?tag=Trending');
+  await expect(page.getByText('Categoria de Prueba')).not.toBeVisible();
+  await page.getByRole('button', { name: 'Popular' }).click();
+  await page.waitForResponse('http://localhost:3000/categories?tag=Popular');
+  await expect(page.getByText('Categoria de Prueba')).not.toBeVisible();
+  await page.getByRole('button', { name: 'Recent' }).click();
+  await page.waitForResponse('http://localhost:3000/categories?tag=Recent');
+  await expect(page.getByText('Categoria de Prueba').first()).toBeVisible();
+})
